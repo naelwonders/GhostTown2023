72a49880e4076bee50bcd12e9e2b311fbfc4857e
Nael Lammens <nslammens@gmail.com>
2023-11-15 09:00:09 +0100

    camera controls and blender boat added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 let renderer, scene, camera;
+let cameraSpeed = 1;
+let cameraX = -50; // on débute à - 100 pour avancer vers max 50
+let cameraY = 15; // haut bas, limite du bas, plane
+let cameraZ = 0; // gauche droite, taille du plane sont les limites
+let mouseX = 0;
+let mouseY = 0;
+let planeWidth = 50;
+let planeHeight = 50;
+let planeMargin = 50;
+
+//blender ship thanks to JamesWhite (https://opengameart.org/content/low-poly-pirate-ship)
+//cute pigeons: https://opengameart.org/content/pigeon-blue-bird-rework
 
 init();
 
 function init() {
     //creation de la scene qui contient tous les elements graphiques (caméra, objets, lumière)
     scene = new THREE.Scene();
+
+    //pour que ma scene s'adapte à ma fenetre 
+    //window.addEventListener('resize', onResize, false); 
 
     // Set the background color of the scene to taupe gray (adjust the RGB values as needed)
     scene.background = new THREE.Color(0x588084);
@@ -13,11 +28,49 @@ function init() {
     //le premier argument est l'angle, généralement  fixée entre 45 et 75
     //les deux derniers c'est le near far on utilisera raisonnablement des valeurs de 0.1 et 100 (avant 0.1, l'objet est invisible, au dela de 100 same thing)
     camera = new THREE.PerspectiveCamera(65, window.innerWidth /
-    window.innerHeight, 1, 100);
-    camera.position.x = -45;
-    camera.position.y = 20;
-    camera.position.z = 0;
-    //on peut aussi positionner la camera en une seule instruction : camera.position.set(-30, 40, 30); 
+    window.innerHeight, 1, 100);   
+
+    // Écouteurs d'événements pour les touches du clavier
+    document.addEventListener('keydown', (event) => {
+        switch (event.key) {
+        case 'ArrowUp':
+            // Avancer
+            cameraX += cameraSpeed;
+            if (cameraX >= 0) cameraX = 0;
+            //console.log(cameraX);
+            break;
+        case 'ArrowDown':
+            // Reculer
+            cameraX -= cameraSpeed;
+            if (cameraX <= -63) cameraX = -63;
+            break;
+        case 'ArrowLeft':
+            // Déplacer vers la gauche
+            cameraZ -= cameraSpeed;
+            if (cameraZ <= -20) cameraZ = -20;
+            break;
+        case 'ArrowRight':
+            // Déplacer vers la droite
+            cameraZ += cameraSpeed;
+            
+            if (cameraZ >= 20) cameraZ = 20;
+            break;
+        }
+    
+        // Mettre à jour la position de la caméra
+        camera.position.set(cameraX, cameraY, cameraZ);
+
+    });
+    
+    
+    camera.position.set(cameraX, cameraY, cameraZ);
+    
+    // document.addEventListener('mousemove', (event) => {
+    //     // Obtenez la position du pointeur de la souris par rapport au centre de la fenêtre
+    //     mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+    //     mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+    //   });
+    // camera.lookAt(mouseX, mouseY, 0);
     camera.lookAt(scene.position); 
 
     //ajouter la camera dans la scene
@@ -31,13 +84,31 @@ function init() {
     //pointLight.add(new THREE.Mesh(pointSphere, new THREE.MeshBasicMaterial({color: 0xccffcc})));
     scene.add(pointLight); 
 
+    //import the boat into my scene (made in blender)
+    const loader = new THREE.OBJLoader();
+        loader.load("/assets/models/ship_wreck.obj", function (loadedMesh) {
+            var boat = new THREE.MeshLambertMaterial({color: 0x5C3A21});
+
+            // loadedMesh is a group of meshes. For
+            // each mesh set the material, and compute the information
+            // three.js needs for rendering.
+            loadedMesh.children.forEach(function (child) {
+                child.material = boat;
+                child.geometry.computeFaceNormals();
+                child.geometry.computeVertexNormals();
+            });
+
+            mesh = loadedMesh;
+            loadedMesh.scale.set(0.1, 0.1, 0.1);
+            //loadedMesh.rotation.x = -0.3;
+            loadedMesh.position.set(-30,0,0);
+            scene.add(loadedMesh);
+        });
+
     //ajout d'une surface plane 
-    let planeWidth = 50;
-    let planeHeight = 50;
-    let planeMargin = 5
     const planeGeometry = new THREE.PlaneGeometry(planeWidth + planeMargin, planeHeight + planeMargin, 1, 1);
     const planeMaterial = new THREE.MeshBasicMaterial({
-        color: 0x4a5e7a
+        color: 0x044872
     });
     const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial); 
     scene.add(planeMesh);
@@ -60,8 +131,8 @@ function init() {
         let z = Math.floor(i / 20) * 2 - planeHeight / 2;
         const geometry = new THREE.BoxGeometry(1, 1, 1)
         //basic material ne reagit pas a la lumière, Lambert reagit
-        const material = new THREE.MeshStandardMaterial({ 
-            color: 0xffffff,
+        const material = new THREE.MeshBasicMaterial({ 
+            color: 0x4a5e7a,
             // map: colorMap, // Assign color (diffuse) map
             // displacementMap: displacementMap, // Assign displacement map
             // normalMap: normalGLMap, // Assign normal map
@@ -71,27 +142,48 @@ function init() {
         scene.add(buildingMesh) 
         buildingMesh.position.x = x; 
         buildingMesh.position.z = z;
-        let sizeBuilding = Math.random()*10
+        let sizeBuilding = Math.random()*15
         buildingMesh.scale.y = sizeBuilding; 
         buildingMesh.position.y = sizeBuilding / 2;
     }
 
+    scene.fog=new THREE.Fog( 0x588084, 0.18, 50); 
+    //scene.fog=new THREE.FogExp2( 0xffffff, 0.01 ); 
 
     // show axes in the screen (pour debugger)
-    const axes = new THREE.AxesHelper(20);
-    scene.add(axes);
+    //const axes = new THREE.AxesHelper(20);
+    //scene.add(axes);
     renderer = new THREE.WebGLRenderer({
         //ca va choper la balise canvas de l'html, la classe que j'ai nommée webgl 
         canvas: document.querySelector('canvas.webgl')
        })
-       renderer.setSize(600, 600);
-       render();
 
+    renderer.setClearColor(0x588084);
+    renderer.setSize(800, 600);
+    render();
     
 }
 
-// Renderer
+// Renderer: c'est l'équivalant de mon update, c'est ici qu'on fait interactivité
 function render(){
+    //mesh.position.x = (Math.cos(step)); c'est pour faire ociller les mesh (maillage donc les petits triangles)
     renderer.render(scene, camera); 
     requestAnimationFrame(render);
+}
+
+function onResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+   } 
+
+function playMusic() {
+    var audioElement = document.getElementById('audio');
+
+    if (audioElement.paused) {
+        audioElement.play(); // Démarre la lecture du son
+    } else {
+        audioElement.pause(); // Met en pause la lecture si le son est déjà en cours de lecture
+        audioElement.currentTime = 0; // Réinitialise la position de lecture au début
+    }
 }
